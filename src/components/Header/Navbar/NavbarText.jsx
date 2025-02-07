b522dfe41894e61c2b6f332064af7100d645423c
gia-grigalashvili <grigalagia07@gmail.com>
2025-02-07 12:51:56 +0400

    change

--- src/components/Header/Navbar/NavbarText.jsx
+++ src/components/Header/Navbar/NavbarText.jsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Hamburger from "hamburger-react";
 import { useNavigate } from "react-router-dom";
+
 export default function NavbarText({ links }) {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
+
   const onSubmit = async () => {
     sessionStorage.removeItem("adminLogin", true);
-
     navigate("/login");
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div>
       <div className="lg:hidden z-50 relative">
@@ -20,10 +33,10 @@ export default function NavbarText({ links }) {
           color="white"
         />
       </div>
-
+      {/* duration-500  */}
       <ul
-        className={`fixed right-0 top-[100px] h-full w-full bg-[#004682] flex flex-col items-center gap-4 p-5 duration-500  ${
-          isOpen ? "translate-x-0" : "translate-x-full "
+        className={`fixed right-0 top-[100px] h-full w-full bg-[#004682] flex flex-col items-center gap-4 p-5  ${
+          isOpen ? "translate-x-0" : "translate-x-full"
         } lg:relative lg:translate-x-0 lg:flex-row lg:w-auto lg:bg-transparent lg:top-0`}
       >
         {links.map((link) => (
@@ -35,7 +48,7 @@ export default function NavbarText({ links }) {
                 window.scrollTo({ top: 0, behavior: "smooth" });
               }}
               className={({ isActive }) =>
-                `py-2 px-4 text-[#C4C4C4] font-extrabold text-[18px] hover:text-[#ff4a4a] transition-transform lg:hover:scale-125 duration-500 ${
+                `py-2 px-4 text-[#f8f8f8] font-extrabold text-[18px] hover:text-[#ff4a4a] transition-transform lg:hover:scale-125 duration-500 ${
                   isActive ? "rounded-lg w-full bg-gray-700" : ""
                 }`
               }
--- src/components/adminLayout/AdminLayout.jsx
+++ src/components/adminLayout/AdminLayout.jsx
@@ -2,10 +2,12 @@ import MainContentWrapper from "../../ui/MainContentWrapper";
 import Header from "../Header/header";
 
 const AdminLayout = ({ children }) => {
+  const isLogined = sessionStorage.getItem("adminLogin");
   return (
     <div>
       <MainContentWrapper>
         {/* header here */}
+
         <Header />
         <main>{children}</main>
         {/* footer here */}
