7e3cb896a4c762e8d2183e2d617b8b4423e982de
gia-grigalashvili <grigalagia07@gmail.com>
2025-02-08 19:19:03 +0400

    add updatebaner

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -1,42 +1,39 @@
 import { useState } from "react";
 import { useGetbanner } from "../../../hooks/useGetBanner";
 import threedots from "/public/imgs/7066144.png";
-
+import { UpdateBanner } from "../../../services/homeServices";
 export default function Banner() {
   const { data, error, isError, isLoading } = useGetbanner();
   const { id, title } = data?.title ? data.title[0] || {} : {};
-  const [showBox, setShowBox] = useState(false); // Controls the visibility of the small box
-  const [showOverlay, setShowOverlay] = useState(false); // Controls the visibility of the overlay
+  const [showBox, setShowBox] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
 
-  // Toggle the small box visibility
   const handleThreedotsClick = () => {
     setShowBox((prev) => !prev);
   };
 
-  // Show the overlay and hide the small box
   const handleChangeClick = () => {
     setShowOverlay(true);
     setShowBox(false);
   };
 
-  // Close the overlay
   const handleCloseOverlay = () => {
     setShowOverlay(false);
   };
 
   if (isLoading) {
-    return <div>Loading...</div>; // Display a loading indicator
+    return <div>Loading...</div>;
   }
 
   if (isError) {
-    return <div>Error: {error.message}</div>; // Display an error message
+    return <div>Error: {error.message}</div>;
   }
 
   return (
     <div>
-      <div className="bg-softBlue bg-[#CBDEEF] break-words mt-[8rem] flex justify-between items-center pt-[3rem] pb-[10rem]">
+      <div className="bg-softBlue bg-[#CBDEEF] break-words mt-[8rem] flex justify-between items-center py-[3.81rem] px-[3.81rem]">
         <div className="text-[4rem]">
-          <h1>{title}</h1>
+          <h1 className="font-extrabold">{title}</h1>
         </div>
         <div className="relative">
           <img
@@ -61,13 +58,14 @@ export default function Banner() {
 
       {showOverlay && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded-lg">
+          <div className="bg-white w-[400px] p-8 rounded-lg">
             <input
               type="text"
               placeholder="Enter new title"
-              className="w-full p-2 border border-gray-300 rounded-lg"
+              className="w-full  p-2 border border-gray-300 rounded-lg"
+              defaultValue={title}
             />
-            <div className="flex justify-between ">
+            <div className="flex mt-[10px] justify-between ">
               <button
                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
                 onClick={handleCloseOverlay}
