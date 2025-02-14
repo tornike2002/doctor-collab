d6a245bec582b7e5c249e6a7d666fd148422d25c
gia-grigalashvili <grigalagia07@gmail.com>
2025-02-14 16:03:43 +0400

    make FooterInfo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Contact from "./Contact";
+import FooterInfo from "./FooterInfo";
 
 export default function Footer() {
   return (
-    <div className="bg-[#EFF6FF] px-[2rem] mt-[8rem]">
+    <div className="bg-[#EFF6FF] flex items-center  justify-between  px-[2rem] mt-[8rem]">
       <Contact />
+      <FooterInfo />
     </div>
   );
 }
