bd00b9998d2704ca08fbca7348fb7ff75b897f59
gia-grigalashvili <grigalagia07@gmail.com>
2025-02-20 18:03:17 +0400

    add oferoverflow

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,12 +11,16 @@ function Modal({ children }) {
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
     modalRoot.appendChild(elRef.current);
+    document.body.style.overflow = "hidden";
 
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      modalRoot.removeChild(elRef.current);
+      document.body.style.overflow = "";
+    };
   }, []);
 
   return createPortal(
-    <div className="fixed z-10 inset-0 bg-black bg-opacity-50 px-[20px] flex justify-center items-center">
+    <div className="fixed z-10 inset-0 bg-black bg-opacity-50 flex justify-center items-center px-5">
       {children}
     </div>,
     elRef.current
