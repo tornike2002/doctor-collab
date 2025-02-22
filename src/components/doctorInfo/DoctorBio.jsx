b8a148e160a0daeb4917c61545f40d222e2f0c34
gia-grigalashvili <grigalagia07@gmail.com>
2025-02-22 23:43:34 +0400

    finish doctorinfo

--- src/components/doctorInfo/DoctorBio.jsx
+++ src/components/doctorInfo/DoctorBio.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import { useGetDoctorIm } from "../../hooks/useGetDoctorIm";
+
 import { useGetDoctorBio } from "../../hooks/useGetDoctorBio";
-import Dcotorgrid from "/public/imgs/Dot Grid.png";
+import DoctorInfo from "./DoctorInfo";
 
 function DoctorBio() {
   const [isOpen, setIsOpen] = useState(false);
-  const { data: doctorimg, isLoading, isError } = useGetDoctorIm();
-  // const { data: doctorinfo } = useGetDoctorBio();
-  // console.log(doctorinfo);
+  const { data: doctorimg, isLoading, isError } = useGetDoctorBio();
+
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
@@ -22,28 +21,13 @@ function DoctorBio() {
 
   return (
     <div className="grid grid-cols-1 pb-10 pt-10">
-      {doctorimg.img.map((doctor) => (
-        <div key={doctor.id} className=" flex  mt-[100px] ">
-          {/* Image with half overlay */}
-          <div className="relative w-[355px] h-[525px] flex justify-center">
-            <img
-              src={doctor.img}
-              className=" top-[-50px] z-10 ml-[50%]  rounded-lg w-[275px] h-[358px] sm:w-[300px] sm:h-[459px] lg:w-[355px] lg:h-[425px]"
-              alt=""
-            />
-          </div>
-
-          {/* Doctor Bio Section */}
-          <div className="relative z-0 flex w-[600px] min-h-[200px] justify-center items-center px-5 md:px-20 bg-[#CCDCF3] mt-[-80px]">
-            {/* {doctorinfo.full_name.map((index) => (
-              <div>
-                <h1>{index.full_name}</h1>
-              </div>
-            ))} */}
-            <h1 className="text-[100px]">{doctor.full_name}</h1>
-            <h1 className="text-[20px]">{doctor.job_description}</h1>
-          </div>
-        </div>
+      {doctorimg.full_name.map((doctor) => (
+        <DoctorInfo
+          img={doctor.img}
+          fullname={doctor.full_name}
+          jobdescription={doctor.job_description}
+          id={doctor.id}
+        />
       ))}
     </div>
   );
--- src/components/doctorInfo/DoctorInfo.jsx
+++ src/components/doctorInfo/DoctorInfo.jsx
@@ -1,12 +1,32 @@
-// import React from "react";
-// import { useGetDoctorBio } from "../../hooks/useGetDoctorBio";
-// export default function DoctorInfo() {
-//   const { data, isLoading } = useGetDoctorBio();
-//   return (
-//     <div>
-//       {data.map((index) => (
-//         <div>{/* <h1>{index}</h1> */}</div>
-//       ))}
-//     </div>
-//   );
-// }
+import React from "react";
+import threedot from "/public/imgs/7066144.png";
+
+export default function DoctorInfo({ img, id, jobdescription, fullname }) {
+  return (
+    <div key={id} className="flex mt-[100px]">
+      <div className="relative w-[355px] h-[525px] flex justify-center">
+        <img
+          src={img}
+          className="top-[-50px] z-10 ml-[50%] rounded-lg w-[275px] h-[358px] sm:w-[300px] sm:h-[459px] lg:w-[355px] lg:h-[425px]"
+          alt=""
+        />
+      </div>
+
+      <div className="relative z-0 flex flex-col items-center  w-[600px] min-h-[200px] px-5 md:px-20 bg-[#CCDCF3] mt-[-80px]">
+        <img
+          className="w-[2rem] absolute cursor-pointer top-[20px] right-[20px]"
+          src={threedot}
+          alt=""
+        />
+
+        {/* Centered Text */}
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="lg:text-[60px] text-[50px] font-sans">{fullname}</h1>
+          <h1 className="lg:text-[35px] text-[20px] text-[#267CC5]">
+            {jobdescription}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
