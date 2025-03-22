edcbfbd023dec851c997d561a439353265c21f6f
gia-grigalashvili <grigalagia07@gmail.com>
2025-03-22 23:39:25 +0400

    add carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Banner from "../components/home/banner/Banner";
 import DoctorBio from "../components/home/doctorInfo/DoctorBio";
 import SeriviceCard from "../components/home/serviciesCard/SeriviceCard";
+import BlogCarousel from "../components/Blog/BlogCarousel/BlogCarousel"
 export default function Home() {
   return (
     <div>
-      <Banner></Banner>
-      <DoctorBio />
+   
+      
       <SeriviceCard />
+      <Banner/>
+      <BlogCarousel/>
+      <DoctorBio />
     </div>
   );
 }
