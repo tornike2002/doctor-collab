0f12b990828342b285a535f5d802abdc3db71b83
tornike2002 <toko.toka2000@gmail.com>
2025-04-11 14:28:08 +0400

    remove unused tags

--- src/components/adminLayout/AdminLayout.jsx
+++ src/components/adminLayout/AdminLayout.jsx
@@ -1,17 +1,13 @@
 import MainContentWrapper from "../../ui/MainContentWrapper";
 import Footer from "../Footer/Footer";
-import Header from "../Header/header";
+import Header from "../Header/Header";
 
 const AdminLayout = ({ children }) => {
-  const isLogined = sessionStorage.getItem("adminLogin");
   return (
     <div>
       <MainContentWrapper>
-       
-
         <Header />
         <main>{children}</main>
-
         <Footer />
       </MainContentWrapper>
     </div>
